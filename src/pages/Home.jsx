dba4f53666fa7aec35da8a37f19b9e2e2c89e228
Jakob Thiberg <jakob.t.thiberg@gmail.com>
2025-10-30 19:34:02 +0100

    Fixed home screen visibility

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,18 @@
 import React from "react";
-import { profilesByInterest } from "../Data/mockProfiles.js";
-import ProfileSection from "../Components/ProfileSection.jsx";
+import { mockProfileData } from "../data/mockProfiles.js";
+import ProfileSection from "../components/ProfileSection.jsx";
 import "../App.css"; // Load global styles here
 
 export default function Home() {
+  // Group profiles by interest
+  const profilesByInterest = mockProfileData.reduce((acc, profile) => {
+    if (!acc[profile.interest]) {
+      acc[profile.interest] = [];
+    }
+    acc[profile.interest].push(profile);
+    return acc;
+  }, {});
+
   return (
     <div>
       {/* Render all interest sections */}
